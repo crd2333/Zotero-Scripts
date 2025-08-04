d4581d35f2e9605f1cd9ce78687af1de85ade84a
crd2333 <113617021+crd2333@users.noreply.github.com>
2025-08-04 13:09:14 +0800

    fix: bug of tag clean

--- 标签清理.js
+++ 标签清理.js
@@ -12,6 +12,8 @@ if (!item) {
 // 1. 清理规则：定义一个数组，包含所有需要被删除的标签。
 const tagToRemove = [
 	"Computer Science - Computer Vision and Pattern Recognition",
+	"Computer Science - Artificial Intelligence",
+	"Computer Science - Machine Learning",
 ];
 
 // 2. 清理规则：定义一个数组，包含所有需要被修改的标签前缀。
@@ -36,10 +38,10 @@ originalTags.forEach(originalTag => {
     let tagChanged = false;
 
 	// 步骤 1: 删除标签
-	if (tagToRemove.hasOwnProperty(currentTag)) {
+	if (tagToRemove.includes(currentTag)) {
 		item.removeTag(currentTag);
 		changes.push(`"${originalTag}" → ""`);
-		return;
+		return; // 跳过后续逻辑
 	}
 
     // 步骤 2: 清理前缀
@@ -47,18 +49,17 @@ originalTags.forEach(originalTag => {
         if (currentTag.startsWith(prefix)) {
             currentTag = currentTag.substring(prefix.length);
             tagChanged = true;
-            break;
+            break; 
         }
     }
 
-    // 步骤 3: 替换标签
-    // 检查清理后的标签是否在替换列表中
+    // 步骤 3: 替换标签，检查清理后的标签是否在替换列表中
     if (tagReplacements.hasOwnProperty(currentTag)) {
         currentTag = tagReplacements[currentTag];
         tagChanged = true;
     }
 
-    // 步骤 3: 应用更改，如果标签经过了清理或替换，则更新条目
+    // 步骤 4: 应用更改，如果标签经过了清理或替换，则更新条目
     if (tagChanged) {
         item.removeTag(originalTag);
         item.addTag(currentTag);
